831157138e71c3617b9c8751e4497c836c68dc6e
lpettenella <luca@Luca-PC>
2023-02-13 08:13:06 +0100

    completed: three mesh testing

--- src/ue_template_assets/src/pages/TestThree.tsx
+++ src/ue_template_assets/src/pages/TestThree.tsx
@@ -1,39 +1,64 @@
 import React, { useEffect, useState } from "react"
 import * as THREE from 'three';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { ActorSubclass } from '@dfinity/agent'
 import { _SERVICE } from '../../../declarations/ue_template/ue_template.did'
 
 function TestThree({actor, principal}: any) {
   const [file, setFile] = useState<File | undefined>(undefined);
+  const [userNfts, setUserNfts] = useState<any[]>([]);
+  const [loader] = useState(new GLTFLoader())
+  const [scene] = useState(new THREE.Scene())
+  const [camera] = useState(new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ))
+  const [renderer] = useState(new THREE.WebGLRenderer())
 
   useEffect(() => {
-    if (!file) return
-
-    const loader = new GLTFLoader();
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  
-    const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.getElementById("mesh-box")!.appendChild( renderer.domElement );
-
-    // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    // const cube = new THREE.Mesh( geometry, material );
-    // scene.add( cube );
-
+  }, [])
+  
+  useEffect(() => {
+    if (!file) return
+    
+    // const loader = new GLTFLoader();
+    // const scene = new THREE.Scene();
+    // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    // const renderer = new THREE.WebGLRenderer();
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 0, 0 )
+    
+    while(scene.children.length > 0){ 
+      scene.remove(scene.children[0]); 
+    }
+    
     async function parseFile() {
       const blob = new Blob([file!], { type: 'application/gltf-binary' })
       const arraybuffer = await blob.arrayBuffer()
       
       loader.parse(arraybuffer, "", (glb) => {
-        console.log(glb)
         scene.add(glb.scene)
       })
     }
-
     parseFile()
+    
+    const light = new THREE.DirectionalLight(0xffffff, 1)
+    light.position.set(2,2,5)
+    scene.add(light)
+    
+    camera.position.z = 5;
+    
+    function animate() {
+      requestAnimationFrame( animate );
+      controls.update()
+      renderer.render( scene, camera );
+    }
+    animate();
+    
+    // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    // const cube = new THREE.Mesh( geometry, material );
+    // scene.add( cube );
 
     // loader.load(
     //   "Chair.glb",
@@ -48,18 +73,6 @@ function TestThree({actor, principal}: any) {
     //     console.log( error );
     //   }
     // );
-
-    const light = new THREE.DirectionalLight(0xffffff, 1)
-    light.position.set(2,2,5)
-    scene.add(light)
-
-    camera.position.z = 5;
-
-    function animate() {
-      requestAnimationFrame( animate );
-      renderer.render( scene, camera );
-    }
-    animate();
   }, [file])
 
   const handleFile = (event: any) => {
@@ -68,9 +81,13 @@ function TestThree({actor, principal}: any) {
     setFile(file!)
   }
 
-  const handleNft = async () => {
+  const handleClick = async () => {
     const res = await actor.getUserTokens(principal)
-    const arraybuffer = res[0].metadata[0].location.InCanister
+    setUserNfts(res)
+  }
+
+  const handleNft = async(nft: any) => {
+    const arraybuffer = nft.metadata[0].location.InCanister
     const file = new File([arraybuffer], "test.glb");
     setFile(file)
   }
@@ -78,7 +95,12 @@ function TestThree({actor, principal}: any) {
   return (
     <>
       {/* <input type="file" onChange={handleFile} /> */}
-      <button onClick={() => handleNft()}>Get NFTs</button>
+      <button onClick={() => handleClick()}>Get NFTs</button>
+      {userNfts.map((nft, key) => 
+        <button key={key} onClick={() => handleNft(nft)}>
+          {nft.metadata[0].name}
+        </button>
+      )}
       <div id="mesh-box"></div>
     </>
   )
